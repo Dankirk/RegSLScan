0d4393fef9a7bcc5c0b377f8bead76131a2fa2ae
Henri Aho <dankirk_88@msn.com>
2019-08-15 19:19:40 +0300

    Change multibyte functions and variables to unicode

--- RegSLScan/RegSlScan.cpp
+++ RegSLScan/RegSlScan.cpp
@@ -1,18 +1,17 @@
-#include <tchar.h>
 #include <AclAPI.h>
 #include <string>
 #include <iostream>
 
 #define MAX_KEY_LENGTH 255
 
-bool QueryKey(HKEY hKey, const PTRUSTEE usersGroup, const std::string& path) {
+bool QueryKey(HKEY hKey, const PTRUSTEEW usersGroup, const std::wstring& path) {
 
 	DWORD cSubKeys = 0;				// number of subkeys 
 	DWORD cbMaxSubKey = MAX_KEY_LENGTH;	// longest subkey size 
 	DWORD cbSecurityDescriptor;		// size of security descriptor 
 
 	// Get subkey count and security descriptor size
-	if (RegQueryInfoKey(
+	if (RegQueryInfoKeyW(
 		hKey,					// key handle 
 		NULL,					// buffer for class name 
 		NULL,					// size of class string 
@@ -41,10 +40,10 @@ bool QueryKey(HKEY hKey, const PTRUSTEE usersGroup, const std::string& path) {
 	ACL *pdacl = (ACL *)((BYTE *)sdr + sdr->Dacl);
 	ACCESS_MASK accessMask;
 
-	if (GetEffectiveRightsFromAcl(pdacl, usersGroup, &accessMask) == ERROR_SUCCESS &&
+	if (GetEffectiveRightsFromAclW(pdacl, usersGroup, &accessMask) == ERROR_SUCCESS &&
 		accessMask & KEY_CREATE_LINK) {
 
-		std::cout << path << std::endl;
+		std::wcout << path << std::endl;
 
 		free(secDesc);
 		return true;
@@ -57,17 +56,17 @@ bool QueryKey(HKEY hKey, const PTRUSTEE usersGroup, const std::string& path) {
 		return false;
 
 	// Allocate resources and traverse subkeys
-	TCHAR* achKey = new TCHAR[cbMaxSubKey + 1];	// buffer for subkey name
+	wchar_t* achKey = new wchar_t[cbMaxSubKey + 1];	// buffer for subkey name
 	DWORD cbName;								// size of name string 
 	DWORD i;
 	DWORD success = 0;
 	HKEY next;
-	std::string newpath;
+	std::wstring newpath;
 
 	for (i = 0; i < cSubKeys; i++) {
 
 		cbName = cbMaxSubKey + 1;
-		if (RegEnumKeyEx(hKey, i, achKey, &cbName, NULL, NULL, NULL, NULL) != ERROR_SUCCESS)
+		if (RegEnumKeyExW(hKey, i, achKey, &cbName, NULL, NULL, NULL, NULL) != ERROR_SUCCESS)
 			continue;
 
 		/*
@@ -77,10 +76,10 @@ bool QueryKey(HKEY hKey, const PTRUSTEE usersGroup, const std::string& path) {
 		*/
 		// std::cout << achKey << std::endl; // For debugging
 
-		if (RegOpenKeyEx(hKey, achKey, 0, KEY_READ, &next) != ERROR_SUCCESS)
+		if (RegOpenKeyExW(hKey, achKey, 0, KEY_READ, &next) != ERROR_SUCCESS)
 			continue;
 				
-		newpath = path + '\\' + achKey;
+		newpath = path + L'\\' + achKey;
 		//std::cout << newpath << std::endl; // For debugging
 		if (QueryKey(next, usersGroup, newpath)) {
 
@@ -88,7 +87,7 @@ bool QueryKey(HKEY hKey, const PTRUSTEE usersGroup, const std::string& path) {
 			success++;
 			if (success >= 5 && success > i) {
 				RegCloseKey(next);
-				std::cout << "Assuming all subkeys in: " << path << " are accessible this way..." << std::endl;
+				std::wcout << L"Assuming all subkeys in: " << path << L" are accessible this way..." << std::endl;
 				break;
 			}
 		}
@@ -99,7 +98,7 @@ bool QueryKey(HKEY hKey, const PTRUSTEE usersGroup, const std::string& path) {
 	return false;
 }
 
-int __cdecl _tmain(int argc, TCHAR* argv[]) {
+int __cdecl wmain(int argc, wchar_t* argv[]) {
 
 	// Get users group sid
 	SID_IDENTIFIER_AUTHORITY sia = SECURITY_NT_AUTHORITY;
@@ -112,33 +111,33 @@ int __cdecl _tmain(int argc, TCHAR* argv[]) {
 		return 0;
 
 	// Build deescriptor we can use to determine user groups access
-	TRUSTEE usersGroup;
-	BuildTrusteeWithSid(&usersGroup, pUsersSid);
+	TRUSTEEW usersGroup;
+	BuildTrusteeWithSidW(&usersGroup, pUsersSid);
 
 	// Open HKEY_LOCAL_MACHINE root key or a sub key defined by command line parameters
 	HKEY hRootKey;
-	std::string path;
+	std::wstring path;
 	LSTATUS result;
 	
 	if (argc >= 2) {
 		path = argv[1];
-		result = RegOpenKeyExA(HKEY_LOCAL_MACHINE, path.c_str(), 0, KEY_READ, &hRootKey);
+		result = RegOpenKeyExW(HKEY_LOCAL_MACHINE, path.c_str(), 0, KEY_READ, &hRootKey);
 	}
 	else {
-		path = TEXT("");
-		result = RegOpenKeyExA(HKEY_LOCAL_MACHINE, NULL, 0, KEY_READ, &hRootKey);
+		path = L"";
+		result = RegOpenKeyExW(HKEY_LOCAL_MACHINE, NULL, 0, KEY_READ, &hRootKey);
 	}
 
 	// Query key and it's subkeys security descriptors recursively 
 	if (result == ERROR_SUCCESS) {
 
-		std::cout << "Searching for keys non-admins can create symbolic links in..." << std::endl;
+		std::wcout << L"Searching for keys non-admins can create symbolic links in..." << std::endl;
 		
 		QueryKey(hRootKey, &usersGroup, path);
 		RegCloseKey(hRootKey);
 	}
 	else
-		std::cout << "Couldn't open root key" << std::endl; 
+		std::wcout << L"Couldn't open root key" << std::endl; 
 
 	// Close resources
 	FreeSid(pUsersSid);
